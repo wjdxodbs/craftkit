ca5b938b45175b81864c4e00e50972a1f206fec7
wjdxodbs <wjdxodbs52@naver.com>
2026-03-26 23:58:15 +0900

    style: 툴 레이아웃 배경 warm dark로 변경

--- app/tools/layout.tsx
+++ app/tools/layout.tsx
@@ -7,9 +7,9 @@ export default function ToolsLayout({
   children: ReactNode
 }) {
   return (
-    <div className="flex min-h-screen bg-[#0d0d1a]">
+    <div className="flex min-h-screen bg-[#111110]">
       <Sidebar />
-      <main className="flex min-w-0 flex-1 flex-col bg-[#111127]">
+      <main className="flex min-w-0 flex-1 flex-col bg-[#191917]">
         <div className="h-14 border-b border-white/10" />
         {children}
       </main>
