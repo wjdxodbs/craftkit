a5f3559a5eb4b8cef5bc239ca5ccc2333d6c57a1
wjdxodbs <wjdxodbs52@naver.com>
2026-03-31 11:03:58 +0900

    style: tools 레이아웃 배경색 CSS 변수로 교체

--- app/tools/layout.tsx
+++ app/tools/layout.tsx
@@ -7,9 +7,9 @@ export default function ToolsLayout({
   children: ReactNode
 }) {
   return (
-    <div className="flex min-h-screen bg-[#111110]">
+    <div className="flex min-h-screen bg-background">
       <Sidebar />
-      <main className="flex min-w-0 flex-1 flex-col bg-[#191917]">
+      <main className="flex min-w-0 flex-1 flex-col bg-background">
         <div className="h-14 border-b border-white/10" />
         {children}
       </main>
